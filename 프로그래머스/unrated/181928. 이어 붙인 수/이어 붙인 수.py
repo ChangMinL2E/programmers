c951bcc2ebb7d28da36a0a8e336f786a52742c6f
ChangMinL2E <109322131+ChangMinL2E@users.noreply.github.com>
2023-07-04 10:00:38 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = 0
+    odd = ''
+    even = ''
+    for num in num_list:
+        if num%2:
+            odd += str(num)
+        else:
+            even += str(num)
+    answer = int(odd) + int(even)
+    
+    return answer
